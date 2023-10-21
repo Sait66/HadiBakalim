606a6c53fffb7c7e546464800099a67fa08a3a73
Sait66 <62934245+Sait66@users.noreply.github.com>
2023-10-21 22:51:24 +0300

    Add files via upload

---
+++ kurtlarsofrasi.fun/kullanici.php
@@ -0,0 +1,15 @@
+<?php
+/*$servername = "localhost";
+$kadi = "your_username";
+$passwd = "your_password";
+$dbname = "saitdb";
+
+$conn = new mysqli($servername, $kadi, $passwd, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}*/
+
+echo '<p> Hello World </p>'
+
+?>
